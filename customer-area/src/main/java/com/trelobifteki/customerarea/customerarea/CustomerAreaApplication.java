b4802b92eeed140b8c866a4cba539341d8680aa9
Lampros Papadimitriou <papadimitriou@tyntec.com>
2018-12-21 08:20:04 +0100

    Adding customer area server

---
+++ customer-area/src/main/java/com/trelobifteki/customerarea/customerarea/CustomerAreaApplication.java
@@ -0,0 +1,14 @@
+package com.trelobifteki.customerarea.customerarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAreaApplication.class, args);
+	}
+
+}
+
