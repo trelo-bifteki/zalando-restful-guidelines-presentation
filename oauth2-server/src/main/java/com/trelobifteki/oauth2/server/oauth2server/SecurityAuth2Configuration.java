d9bb73b3a767aec9d0214a1d6c00989b56556e35
Lampros Papadimitriou <lampros.papadimitriou@check24.de>
2018-12-21 15:38:02 +0100

    Update oauth2 configuration

--- oauth2-server/src/main/java/com/trelobifteki/oauth2/server/oauth2server/SecurityAuth2Configuration.java
+++ oauth2-server/src/main/java/com/trelobifteki/oauth2/server/oauth2server/SecurityAuth2Configuration.java
@@ -33,7 +33,7 @@ public class SecurityAuth2Configuration extends AuthorizationServerConfigurerAda
                        .accessTokenValiditySeconds(2000)        // expire time for access token
                        .refreshTokenValiditySeconds(-1)         // expire time for refresh token
                .scopes("read", "write")                         // scope related to resource server
-                       .authorizedGrantTypes("password", "refresh_token");      // grant type
+                       .authorizedGrantTypes("password", "refresh_token", "customer-area.customer-cases");      // grant type
     }
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
